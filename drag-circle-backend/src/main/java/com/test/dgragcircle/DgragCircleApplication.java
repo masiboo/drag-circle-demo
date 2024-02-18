8a93e33cf5b0decf4732090f036743823d68ee92
Masi boo <masiboo@gmail.com>
2024-02-18 17:45:39 +0100

    Add generated spring boot backend project

---
+++ drag-circle-backend/src/main/java/com/test/dgragcircle/DgragCircleApplication.java
@@ -0,0 +1,13 @@
+package com.test.dgragcircle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DgragCircleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DgragCircleApplication.class, args);
+	}
+
+}
---
+++ drag-circle-backend/src/test/java/com/test/dgragcircle/DgragCircleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.dgragcircle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DgragCircleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
