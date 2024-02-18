7b59a45f182928a833c5e67cb4bc051428a2fc80
Masi boo <masiboo@gmail.com>
2024-02-18 18:13:46 +0100

    Add CircleInfo model

---
+++ drag-circle-backend/src/main/java/com/test/dgragcircle/model/CircleInfo.java
@@ -0,0 +1,11 @@
+package com.test.dgragcircle.model;
+
+
+import lombok.Data;
+
+@Data
+public class CircleInfo {
+    private int componentId;
+    private int coordinateX;
+    private int coordinateY;
+}
