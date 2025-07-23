90d001657a87d3ad4e4b1aed3ce799ea07115d9d
iprosoft-nl <masum.islam2@gmail.com>
2025-07-23 09:53:20 +0200

    Implement missing backend classes

---
+++ drag-circle-backend/src/main/java/com/test/dgragcircle/repository/CircleInfoRepository.java
@@ -0,0 +1,8 @@
+package com.test.dgragcircle.repository;
+
+
+import com.test.dgragcircle.model.CircleInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CircleInfoRepository extends JpaRepository<CircleInfo, Long > {
+}
